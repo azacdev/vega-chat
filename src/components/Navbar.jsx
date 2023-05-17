9a4d39f1b7253ec0fe5411120c5d704ff79e32c1
azacdev <azacdev@gmail.com>
2023-05-17 22:54:10 +0100

    update

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+      <div className="navbar bg-neutral text-neutral-content">
+        <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
---
+++ src/pages/Login.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Login = () => {
+  return (
+    <div className="hero min-h-screen bg-base-200">
+      <div className="hero-content text-center">
+        <div className="max-w-md">
+          <h1 className="text-5xl font-bold">Hello there </h1>
+          <p className="py-6">
+            Join the conversation, meet new people, and make connections in one shared room.
+          </p>
+          <button className="btn btn-primary">Login with Google</button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Login
