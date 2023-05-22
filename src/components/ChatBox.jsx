07690f7395f59e1ee61e0412c7719e59e8637a49
azacdev <azacdev@gmail.com>
2023-05-23 00:51:37 +0100

    update

--- src/components/ChatBox.jsx
+++ src/components/ChatBox.jsx
@@ -1,26 +1,39 @@
-import Message from "./Message"
+import { useEffect } from "react";
+import Message from "./Message";
+import { collection, query, where, onSnapshot } from "firebase/firestore";
 
 const ChatBox = () => {
   const messages = [
     {
       id: 1,
       text: "Hello There Anakin",
-      name: "Azacdev"
+      name: "Azacdev",
     },
     {
       id: 2,
       text: "Now you'll die Obiwan",
-      name: "Monarch"
-    }
-  ]
+      name: "Monarch",
+    },
+  ];
+
+  useEffect(() => {
+    const q = query(collection(db, "cities"), where("state", "==", "CA"));
+    const unsubscribe = onSnapshot(q, (querySnapshot) => {
+      const cities = [];
+      querySnapshot.forEach((doc) => {
+        cities.push(doc.data().name);
+      });
+      console.log("Current cities in CA: ", cities.join(", "));
+    });
+  });
 
   return (
     <div className="pb-44 pt-20 containerWrap">
-      {messages.map(message => (
-       <Message key={message.id} message={message}/>
+      {messages.map((message) => (
+        <Message key={message.id} message={message} />
       ))}
     </div>
-  )
-}
+  );
+};
 
-export default ChatBox
+export default ChatBox;
