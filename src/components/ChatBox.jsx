5b77c2dd38bab2e3d9505ccaa15e0b7ed229a264
azacdev <azacdev@gmail.com>
2023-05-23 18:32:07 +0100

    update

--- src/components/ChatBox.jsx
+++ src/components/ChatBox.jsx
@@ -1,21 +1,10 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import Message from "./Message";
 import { collection, query, onSnapshot } from "firebase/firestore";
 import { db } from "../firebase";
 
 const ChatBox = () => {
-  const messages = [
-    {
-      id: 1,
-      text: "Hello There Anakin",
-      name: "Azacdev",
-    },
-    {
-      id: 2,
-      text: "Now you'll die Obiwan",
-      name: "Monarch",
-    },
-  ];
+  const [messages, setMessages] = useState([])
 
   useEffect(() => {
     const q = query(
@@ -24,12 +13,13 @@ const ChatBox = () => {
     const unsubscribe = onSnapshot(q, (querySnapshot) => {
       const messages = [];
       querySnapshot.forEach((doc) => {
-        // messages.push(doc.data().name);
-        console.log(doc.data());
+        messages.push({...doc.data(), id: doc.id});
       });
+      setMessages(messages)
     });
+    return () => unsubscribe
   }, []);
-
+  
   return (
     <div className="pb-44 pt-20 containerWrap">
       {messages.map((message) => (
