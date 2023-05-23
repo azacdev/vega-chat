5ec0f7ca1517c96691289204c8ad05966eac0364
azacdev <azacdev@gmail.com>
2023-05-23 01:03:42 +0100

    update

--- src/components/ChatBox.jsx
+++ src/components/ChatBox.jsx
@@ -1,6 +1,7 @@
 import { useEffect } from "react";
 import Message from "./Message";
-import { collection, query, where, onSnapshot } from "firebase/firestore";
+import { collection, query, onSnapshot } from "firebase/firestore";
+import { db } from "../firebase";
 
 const ChatBox = () => {
   const messages = [
@@ -17,15 +18,17 @@ const ChatBox = () => {
   ];
 
   useEffect(() => {
-    const q = query(collection(db, "cities"), where("state", "==", "CA"));
+    const q = query(
+      collection(db, "messages")
+    );
     const unsubscribe = onSnapshot(q, (querySnapshot) => {
-      const cities = [];
+      const messages = [];
       querySnapshot.forEach((doc) => {
-        cities.push(doc.data().name);
+        // messages.push(doc.data().name);
+        console.log(doc.data());
       });
-      console.log("Current cities in CA: ", cities.join(", "));
     });
-  });
+  }, []);
 
   return (
     <div className="pb-44 pt-20 containerWrap">
